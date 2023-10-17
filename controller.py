915c63e71b08355099422e275fe31e07bb7b5ad2
Arek Cios <arek123113@gmail.com>
2023-10-17 17:54:06 +0200

    Feature: add separate write methods to controller

--- controller.py
+++ controller.py
@@ -14,7 +14,6 @@ class status:
         except:
             print(f"ERROR: couldnt load {self.path} file.")
 
-#tempporary fix want 'temp' to be updated from pi.
     def write(self, light_1, temp_set):
         try:
             status = {'light_1': int(light_1), 'temp_set': temp_set, 'temp': 0}
@@ -22,3 +21,31 @@ class status:
                 json.dump(status, json_file)
         except:
             print(f"ERROR: couldnt update {self.path} file")
+
+    def write_light(self, light1):
+        try:
+            data = self.read()
+            data['light_1'] = int(light1)
+            with open(self.path, 'w') as json_file:
+                json.dump(data, json_file)
+        except:
+            print(f"ERROR: couldnt update {self.path} file with lamp data.")
+
+    def write_temp_setting(self, temp_set):
+        try:
+            data = self.read()
+            data['temp_set'] = int(temp_set)
+            with open(self.path, 'w') as json_file:
+                json.dump(data, json_file)
+        except:
+            print(f"ERROR: couldnt update {self.path} file with temperature setting data.")
+
+    def write_temp(self, temp):
+        try:
+            data = self.read()
+            data['temp'] = int(temp)
+            with open(self.path, 'w') as json_file:
+                json.dump(data, json_file)
+        except:
+            print(f"ERROR: couldnt update {self.path} file with temperature data.")
+            
