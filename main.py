b5c2fe7de9117cc969cf778eadde92c5a17bfb94
Arek Cios <arek123113@gmail.com>
2023-10-17 18:10:53 +0200

    Add api endpoints for write methods from status controller.

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ statusController = status("status.json")
 parser = reqparse.RequestParser()
 parser.add_argument('light_1')
 parser.add_argument('temp_set')
+parser.add_argument('temp')
 parser.add_argument('API_KEY')
 
 
@@ -29,8 +30,50 @@ class Home(Resource):
             del args["API_KEY"]
             return args
         return "API_KEY missing."
-        
+
+class Light(Resource):
+    def post(self):
+        args = parser.parse_args()
+        if args['API_KEY'] == API_KEY:
+            statusController.write_light(args['light_1'])
+
+            del args["API_KEY"]
+            del args["temp_set"]
+            del args["temp"]
+
+            return args
+        return "API_KEY missing."
+    
+class TempSetting(Resource):
+    def post(self):
+        args = parser.parse_args()
+        if args['API_KEY'] == API_KEY:
+            statusController.write_temp_setting(args["temp_set"])
+
+            del args["API_KEY"]
+            del args["light_1"]
+            del args["temp"]
+
+            return args
+        return "API_KEY missing."
+
+class Temp(Resource):
+    def post(self):
+        args = parser.parse_args()
+        if args['API_KEY'] == API_KEY:
+            statusController.write_temp(args["temp"])
+
+            del args["API_KEY"]
+            del args["light_1"]
+            del args["temp_set"]
+
+            return args
+        return "API_KEY missing."
+
 api.add_resource(Home, "/")
+api.add_resource(Light, "/light")
+api.add_resource(TempSetting, "/temp-set")
+api.add_resource(Temp, "/temp")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
